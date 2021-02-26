b25fe0e6f8c0343e39622ac2efeb3c7e6c875473
Daniil <dr.chashka@gmail.com>
2021-02-26 14:41:04 +0300

    Доработка FlipByte

---
+++ flipbyte/flipbyte/Tests.bat
@@ -0,0 +1,79 @@
+@echo off
+
+REM Путь к тестируемой программе передаётся через 1-й аргумент командной строки
+SET PROGRAM="%~1"
+
+REM Защита от запуска без аргумента, задающего пусть к программе
+if %PROGRAM%=="" (
+	echo Please specify file path to the program
+)
+
+REM Блок с тестами
+REM The lowest acceptable limit
+%PROGRAM% 0 > %TEMP%\test_1.txt
+if ERRORLEVEL 1 goto err
+fc test_1.txt %TEMP%\test_1.txt
+if ERRORLEVEL 1 goto err
+
+REM The lowest unacceptable limit
+%PROGRAM% -1 > %TEMP%\test_2.txt
+if ERRORLEVEL 1 goto err
+fc test_2.txt %TEMP%\test_2.txt
+if ERRORLEVEL 1 goto err
+
+REM The highest acceptable limit
+%PROGRAM% 255 > %TEMP%\test_3.txt
+if ERRORLEVEL 1 goto err
+fc test_3.txt %TEMP%\test_3.txt
+if ERRORLEVEL 1 goto err
+
+REM The highest unacceptable limit
+%PROGRAM% 256 > %TEMP%\test_4.txt
+if ERRORLEVEL 1 goto err
+fc test_4.txt %TEMP%\test_4.txt
+if ERRORLEVEL 1 goto err
+
+REM Unacceptable amount of arguments
+%PROGRAM% 123 with words > %TEMP%\test_5.txt
+if ERRORLEVEL 1 goto err
+fc test_5.txt %TEMP%\test_5.txt
+if ERRORLEVEL 1 goto err
+
+REM Unacceptable value (string)
+%PROGRAM% "" > %TEMP%\test_6.txt
+if ERRORLEVEL 1 goto err
+fc test_6.txt %TEMP%\test_6.txt
+if ERRORLEVEL 1 goto err\
+
+REM Unacceptable value (float)
+%PROGRAM% 3.14 > %TEMP%\test_7.txt
+if ERRORLEVEL 1 goto err
+fc test_7.txt %TEMP%\test_7.txt
+if ERRORLEVEL 1 goto err
+
+REM Unacceptable value (string)
+%PROGRAM% word > %TEMP%\test_8.txt
+if ERRORLEVEL 1 goto err
+fc test_8.txt %TEMP%\test_8
+if ERRORLEVEL 1 goto err
+
+REM Unacceptable value (int with string)
+%PROGRAM% 2withwords > %TEMP%\test_9.txt
+if ERRORLEVEL 1 goto err
+fc test_9.txt %TEMP%\test_9.txt
+if ERRORLEVEL 1 goto err
+
+REM Unacceptable value (int with string)
+%PROGRAM% wordswith2 > %TEMP%\test_10.txt
+if ERRORLEVEL 1 goto err
+fc test_10.txt %TEMP%\test_10
+if ERRORLEVEL 1 goto err
+
+REM Тесты прошли успешно
+echo All tests passed successfuly
+exit /B 0
+
+REM Сюда попадаем в случае ошибки
+:err
+echo Test failed
+exit /B 1
--- flipbyte/flipbyte/flipbyte.cpp
+++ flipbyte/flipbyte/flipbyte.cpp
@@ -2,52 +2,82 @@
 #include <string>
 #include <optional>
 
-std::optional<unsigned int> ParseDecNum(int argc, char* argv[])
+std::optional<int> ParseDecNum(int argc, char* argv[])
 {
     if (argc != 2)
     {
         std::cout << "Invalid arguments count\n";
-        std::cout << "Usage: flipbyte.exe <input byte>\n";
+        std::cout << "Usage: flipbyte.exe <input byte>";
         return std::nullopt;
     }
 
     std::string inputString = argv[1];
 
     size_t pos;
-    unsigned int decNum = std::stoi(inputString, &pos);
+    uint8_t byte;
+    try 
+    {
+        byte = static_cast<uint8_t>(std::stoi(inputString, &pos));
+    }
+    catch (const std::invalid_argument&)
+    {
+        pos = std::string::npos;
+    }
 
     if (pos != inputString.length())
     {
         std::cout << "Invalid argument was given\n";
-        std::cout << "Usage: flipbyte.exe <input byte>\n";
+        std::cout << "Usage: flipbyte.exe <input byte>";
         return std::nullopt;
     }
 
-    return decNum;
+    return byte;
 }
 
-unsigned int flipByte(unsigned int &decNum)
+/*
+
+ABCDEFGH
+EFGHABCD меняем сосендние 4-ки битов
+GHEFCDAB меняем сосендние пары битов
+HGFEDCBA меняем сосендние пары биты
+*/
+
+
+uint8_t FlipByte(uint8_t byte)
 {
-    unsigned int firstPart = (decNum & 0x0f) << 4;
-    unsigned int secondPart = (decNum & 0xf0) >> 4;
-    return secondPart | firstPart;
+    uint8_t result = 0;
+    uint8_t n;
+    for (int i = 0; i <= 7; ++i)
+    {
+        n = byte & (1 << i);
+        if (i < 4)
+        {
+            n = n << (7 - 2 * i);
+        }
+        else
+        {
+            n = n >> (2 * i - 7);
+        }
+        result |= n;
+    }
+    return result;
 }
 
 int main(int argc, char *argv[])
 {
-    auto decNum = ParseDecNum(argc, argv);
+    auto byte = ParseDecNum(argc, argv);
 
-    if (!decNum)
+    if (!byte)
     {
         return 1;
     }
     
-    if (decNum > 255 || decNum < 0)
+    if (byte > 255 || byte < 0)
     {
-        std::cout << "The input number not in acceptable range (0-255).\n";
+        std::cout << "The input number not in acceptable range (0-255).";
         return 1;
     }
 
-    std::cout << flipByte(decNum.value()) << "\n";
+    std::cout << static_cast<int>(FlipByte(byte.value())) << "\n";
     return 0;
 }
