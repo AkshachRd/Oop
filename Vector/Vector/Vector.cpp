008c34fae63e66d8ad3e52fe8eb007d39187b2bc
Daniil <dr.chashka@gmail.com>
2021-03-18 18:18:56 +0300

    Разработка Vector

---
+++ Vector/Vector/Vector.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <optional>
+
+bool ParseVector(std::istream& inStrm, std::vector<float>& vec)
+{
+	std::string str;
+
+	while (inStrm >> str)
+	{
+		std::istringstream strm(str);
+		float num;
+		if (strm >> num)
+		{
+			vec.push_back(num);
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	return !vec.empty();
+}
+
+bool ProcessVector(std::vector<float>& vec)
+{
+	auto max = std::max_element(std::begin(vec), std::end(vec));
+	auto min = std::min_element(std::begin(vec), std::end(vec));
+
+	std::for_each(vec.begin(), vec.end(), [](auto max, auto min) { ; };
+
+	return true;
+}
+
+//6 вариант
+int main()
+{
+	
+
+	return 0;
+}
