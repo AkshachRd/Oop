2ab1bc49ef48721494ef5fb79bdc7bd4f959a4a6
Daniil <dr.chashka@gmail.com>
2021-03-25 14:20:03 +0300

    Разработан Vector

--- Vector/Vector/Vector.cpp
+++ Vector/Vector/Vector.cpp
@@ -1,45 +1,31 @@
 ﻿#include <iostream>
-#include <sstream>
-#include <string>
 #include <vector>
-#include <algorithm>
-#include <optional>
-
-bool ParseVector(std::istream& inStrm, std::vector<float>& vec)
-{
-	std::string str;
-
-	while (inStrm >> str)
-	{
-		std::istringstream strm(str);
-		float num;
-		if (strm >> num)
-		{
-			vec.push_back(num);
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	return !vec.empty();
-}
-
-bool ProcessVector(std::vector<float>& vec)
-{
-	auto max = std::max_element(std::begin(vec), std::end(vec));
-	auto min = std::min_element(std::begin(vec), std::end(vec));
-
-	std::for_each(vec.begin(), vec.end(), [](auto max, auto min) { ; };
-
-	return true;
-}
+#include "Vector.h"
 
 //6 вариант
 int main()
 {
+	std::vector<float> inputVector;
+
+	if (!ParseVector(std::cin, inputVector))
+	{
+		std::cout << "Invalid input data or empty input" << std::endl;
+		return 1;
+	}
+
+	if (inputVector.empty())
+	{
+		return 0;
+	}
+
+	// Multiple every vector element by max element and divide every vector element by min element
+	ProcessVector(inputVector);
 	
+	// Vector sorting
+	std::sort(inputVector.begin(), inputVector.end());
+
+	// Copying vector to the output
+	copy(inputVector.begin(), inputVector.end(), std::ostream_iterator<float>(std::cout, " "));
 
 	return 0;
 }
